a21a6b16fda27007bebf9b48fe721de1ffcd2c02
Sonia <sonia.sun16@gmail.com>
2024-09-06 12:25:09 +0800

    Crud task practice

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,48 +1,92 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+import Button from './components/Button.vue'
+
+const filterText = ref("")
+const nameList = ref(["Emil, Hans", "Mustermann, Max", "Tisch, Roman"])
+const selectedFirst = ref("")
+const selectedLast = ref("")
+const selected = ref("")
+
+const filteredNames = computed(() => {
+  return nameList.value.filter((name) => 
+    name.split(', ')[0].startsWith(filterText.value)
+  )
+})
+
+const resetSelectedFields = () => {
+  selectedFirst.value = ""
+  selectedLast.value = ""
+}
+
+const create = () => {
+  nameList.value.push(selectedLast.value + ", " + selectedFirst.value)
+  resetSelectedFields()
+}
+
+const update = () => {
+  const updateIndex = nameList.value.indexOf(selected.value)
+  nameList.value[updateIndex] = selectedLast.value + ", " + selectedFirst.value
+  selected.value = selectedLast.value + ", " + selectedFirst.value
+}
+
+const remove = () => {
+  const deleteIndex = nameList.value.indexOf(selected.value)
+  nameList.value.splice(deleteIndex, 1)
+  resetSelectedFields()
+}
+
+const updateInputOnSelect = (selectedName) => {
+  [selectedLast.value, selectedFirst.value] = selectedName.split(', ')
+  selected.value = selectedName
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="hellooo vue test" />
-      test {{ 1+1 }}
+    <div>
+      <input placeholder="Filter prefix" v-model="filterText">
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <select size="5" @change="(event) => updateInputOnSelect(event.target.value)">
+      <option v-for="name in filteredNames">
+        {{ name }}
+      </option>
+    </select>
+
+    <label for="FirstName">
+      Name:
+      <input id="FirstName" v-model="selectedFirst">
+    </label>
+
+    <label for="LastName">
+      Surname:
+      <input id="LastName" v-model="selectedLast">
+    </label>
+
+    <div class="buttons">
+      <Button text="Create" @click="create"/>
+      <Button text="Update" @click="update"/>
+      <Button text="Delete" @click="remove"/>
+    </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
+input {
   display: block;
-  margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+select {
+  margin: 10px 15px 0px 0px;
+  width: 200px;
+  float: left;
 }
+
+.buttons {
+  display: flex;
+  gap: 5px;
+  margin-top: 20px;
+}
+
 </style>
---
+++ vue_test/src/components/Button.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+   <button>{{ text }}</button>
+</template>
